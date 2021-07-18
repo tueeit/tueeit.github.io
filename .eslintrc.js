2f8a15386ecc51f918f5abf5923f798ddb9ec601
Bruno Andersen Chen <bruno.chen@tueeit.org.tw>
2021-07-18 16:42:35 +0800

    chore: Install and configure linting tool

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'next', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    '@next/next/no-img-element': 'off',
+    'func-style': ['error', 'declaration'],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'prettier/prettier': 'error',
+  },
+};
